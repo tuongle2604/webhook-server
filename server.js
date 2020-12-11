0f5a71381fb0c94c8c8e76f46669b0ed7fb6cb54
tuongle2604 <tuongle2604@gmail.com>
2020-12-11 13:58:32 +0000

    handle hook

--- server.js
+++ server.js
@@ -6,19 +6,21 @@ const app = express();
 app.use(bodyParser.json());
 
 app.post("/webhook", (req, res) => {
-  rebuild(req.body.repository.name);
+  // rebuild(req.body.repository.name);
+  setTimeout(() => rebuild(req.body.repository.name), 1000);
   res.status(200).end();
 })
 
 function rebuild(projectName) {
-  return new Promise((ok, no) => {
-    shell.exec("echo rebuild project: " + projectName);
-    shell.cd(`../${projectName}`);
-    shell.exec("git pull origin master");
-    shell.exec("npm install");
-    shell.exec("npm run build");
-    ok();
-  })
+  console.log("rebuild project: " + projectName);
+  shell.cd(`../${projectName}`);
+  console.log("Pull code ...");
+  shell.exec("git pull origin master");
+  console.log("Installing package...");
+  shell.exec("npm install", {silent:true});
+  console.log("Building...");
+  shell.exec("npm run build", {silent:true});
+  console.log("Done!");
 }
 
 app.listen(3000);
