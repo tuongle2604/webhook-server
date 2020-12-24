022d821673e29b2eeef77d2c9aa489c148a00f72
tuongle2604 <tuongle2604@gmail.com>
2020-12-25 00:53:55 +0700

    update code: not rebuild if only change readme.md

--- server.js
+++ server.js
@@ -5,6 +5,16 @@ const { rebuild } = require("./helper");
 app.use(bodyParser.json());
 
 app.post("/webhook", (req, res) => {
+  const modifiedFiles = req.body.head_commit.modified;
+  // Not rebuild if only change readme.md
+  if (
+    modifiedFiles.length === 1 &&
+    modifiedFiles[0].toLowerCase() === "readme.md"
+  ) {
+    return res.status(200).end();
+  }
+
+  const projectName = req.body.repository.name;
   setTimeout(() => rebuild(req.body.repository.name), 0);
   res.status(200).end();
 });
